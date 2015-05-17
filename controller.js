320ee0760bb0949882df467b80d8b16fbc0f1572
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-05-17 14:31:00 -0600

    got the list to start working

--- controller.js
+++ controller.js
@@ -1,4 +1,12 @@
 
 nameApp.controller("NameCtrl", function ($scope) {
   $scope.names = ["Smith", "Jason", "Derude"];
+  $scope.addName = function() {
+    $scope.names.push($scope.enteredName);
+    $scope.enteredName = "";
+    };
+    $scope.removeName = function(name) {
+      var i = $scope.names.indexOf(name);
+      $scope.names.splice(i, 1);
+    };
 });
